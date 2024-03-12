d420ecdf306dcf5b5d6eeb63b7572e177c6f06a1
liangjiongxin <liangjiongxin@baidu.com>
2024-03-12 13:27:06 +0800

    feature: add limit

--- src/app/_components/prompt/prompt-input.tsx
+++ src/app/_components/prompt/prompt-input.tsx
@@ -2,25 +2,35 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Skeleton } from "@/components/ui/skeleton";
+import { AppContext } from "@/contexts/AppContext";
 import { Confetti } from "@/lib/confetti";
 import { Loader2, LoaderCircle } from "lucide-react";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 type Props = {};
 
 export default function PromptInput({}: Props) {
+  const { userStatus } = useContext(AppContext);
   const [inputV, setInputValue] = useState("");
   const [loading, setLoading] = useState(false);
   const [imgUrl, setImgUrl] = useState("");
   const handleInput = () => {
     toast.success(inputV);
   };
+  //
+  const [limit, setLimit] = useState(2);
 
   // generate photo
   async function generatePhoto(prompt: string) {
     try {
+      if (userStatus?.status === "unsubscribed" || limit == 0) {
+        toast.error(
+          "You are not subscribed and use out of quota, please try again later",
+        );
+        return;
+      }
       toast.success(`Prompt: inputV`);
       await new Promise((resolve) => setTimeout(resolve, 500));
       setLoading(true);
@@ -39,6 +49,8 @@ export default function PromptInput({}: Props) {
         toast.error(errorMessage);
       }
       setImgUrl(newPhoto?.imgUrl[0]);
+      setLimit((prev) => prev - 1);
+      toast.success(`Total free credit 2, remaining credit ${limit}`);
     } catch (error) {
       toast.error("error generating image");
     } finally {
@@ -58,7 +70,7 @@ export default function PromptInput({}: Props) {
           variant="secondary"
           className="flex gap-2 duration-150 hover:bg-slate-100 hover:text-primary"
           onClick={() => generatePhoto(inputV)}
-          disabled={loading}
+          disabled={loading || limit === 0}
         >
           {loading && <Loader2 className="h-4 w-4 animate-spin" />}
           Logo It
@@ -70,6 +82,8 @@ export default function PromptInput({}: Props) {
           <LoaderCircle className="h-8 w-8 animate-spin stroke-orange-50 " />
         </Skeleton>
       )}
+
+      <p className="self-center font-semibold">Free Quota {limit}</p>
 
       {imgUrl && (
         <Image
